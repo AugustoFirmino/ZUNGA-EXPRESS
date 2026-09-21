8f2e54240dbb9e2a7e426241b458d1ad0b376fe9
Augusto Firmino <gugufirmino1l@gmail.com>
2026-09-21 10:19:37 +0100

    Corrigir controller de admin

--- servidor/config/database.js
+++ servidor/config/database.js
@@ -1,26 +1,32 @@
 import mysql from "mysql2/promise";
 
-
 const pool = mysql.createPool({
-
     host: process.env.DB_HOST,
-
     user: process.env.DB_USER,
-
     password: process.env.DB_PASSWORD,
-
     database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT) || 3306,
 
-    port: process.env.DB_PORT,
-
-    waitForConnections:true,
-
-    connectionLimit:10,
-
-    queueLimit:0
-
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
+// TESTE DE CONEXÃO
+pool.getConnection()
+    .then(connection => {
+        console.log("========================================");
+        console.log("MYSQL CONECTADO COM SUCESSO");
+        console.log("========================================");
 
+        connection.release();
+    })
+    .catch(error => {
+        console.error("========================================");
+        console.error("ERRO AO CONECTAR MYSQL");
+        console.error("Código:", error.code);
+        console.error("Mensagem:", error.message);
+        console.error("========================================");
+    });
 
 export default pool;
